1cec2e733ab4c0f6a243f4f3ebb2f78a0cfad053
alejandro lab <shilab@C060600.forest.usf.edu>
2019-06-27 15:35:44 -0400

    finished first version of the polar cam module

--- PCAM.py
+++ PCAM.py
@@ -4,16 +4,50 @@ import numpy as np
 class PCAM:
     def __init__(self, camID = 0, CTI = 'C:/Program Files/MATRIX VISION/mvIMPACT Acquire/bin/x64/mvGenTLProducer.cti'):
         self.camID = camID
-        self.h = Harvester()
-        self.h.add_cti_file(CTI)
-        self.h.update_device_info_list()
-        self.ia = self.h.create_image_acquirer(camID)
+        self.H = Harvester()
+        self.H.add_cti_file(CTI)
+        self.H.update_device_info_list()
+        self.ia = self.H.create_image_acquirer(camID)
+        self.x = self.ia.device.node_map.OffsetX
+        self.y = self.ia.device.node_map.OffsetY
+        self.w = self.ia.device.node_map.Width
+        self.h = self.ia.device.node_map.Height
+
+    def __del__(self):
+        print('heyy')
+        self.buffer.queue()
+        print('hey')
+        self.ia.stop_image_acquisition()
+        print('hey2')
+        self.ia.destroy()
+        print('hey3')
+        self.H.reset()
     
     def set_pixel_format(self, format):
         self.ia.device.node_map.PixelFormat.value = format
+
+    def set_size(self, w, h):
+        self.w = w
+        self.h = h
+        self.ia.device.node_map.Width = w
+        self.ia.device.node_map.Height = h
     
-    def set_frame_rate(self, rate):
-        self.ia.device.node_map.
+    def set_offset(self, x, y):
+        self.x = x
+        self.y = y
+        self.ia.device.node_map.OffsetX = x
+        self.ia.device.node_map.OffsetY = y
+
+    def set_exposure(self, exposure):
+        self.exposure = exposure
+        self.ia.device.node_map.OffsetY
 
     def start(self):
+        self.ia.start_image_acquisition()
 
+    def fetch_buffer(self):
+        self.buffer = self.ia.fetch_buffer()
+        return self.buffer.payload.components[0].data.reshape(self.w, self.h)
+
+    def queue_buffer(self):
+        self.buffer.queue()
--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import numpy as np
 import pyglet
 import SLM
+import PCAM
+
+pcam = PCAM.PCAM()
 
 slm = SLM.SLM(1)
 slm2 = SLM.SLM(3)
+
+pcam.set_pixel_format('Mono12')
+pcam.set_offset(0, 0)
+pcam.set_size(256, 256)
+
+pcam.start()
 
 def update(dt):
     img = np.random.choice([0, 1], size = (64, 64), p=[0.9, 0.1])
     slm.set_location_center(slm.screen_width/2, slm.screen_height/2, 512, 512)
     slm.set_array(img)
     slm2.set_location_center(slm2.screen_width/2, slm2.screen_height/2, 512*2, 512*2)
-    slm2.set_array(img)
+    data = pcam.fetch_buffer()
+    slm2.set_array(data/255)
+    pcam.queue_buffer()
 
-pyglet.clock.schedule_interval(update, 1/30.0)
+pyglet.clock.schedule_interval(update, 1/60.0)
 
 pyglet.app.run()
+
+print('hey')
